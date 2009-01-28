4be9e4ce0d2af50d62727f1393927a9577d2eba6
rfranke <rfranke@7ce873d0-865f-4ce7-a662-4bb36ea78beb>
2009-01-28 13:14:28 +0000

    add distclean.bat to clean up for a distribution

--- Modelica_Fluid/clean.bat
+++ Modelica_Fluid/clean.bat
@@ -1,3 +1,5 @@
-del /F /S *.mat buildlog.txt ds*.txt *.c dymosim.exe
+rem delete temporary files that should not be added to the SVN
+
+del /F /S *.mat buildlog.txt ds*.txt *.c dymosim.exe request stop
 del /F /S *.mof
 del /F /S *.bak-mo
